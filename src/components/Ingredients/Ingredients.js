ec7ba4666c550011932877e36e1e95385b86c84e
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-06-30 14:15:27 -0400

    Hooks Basics

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -7,20 +7,29 @@ import Search from './Search';
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
-  const addIngredientHandler = ingredient => {
-    setUserIngredients(prevIngredients => [
+  const addIngredientHandler = (ingredient) => {
+    setUserIngredients((prevIngredients) => [
       ...prevIngredients,
-      { id: Math.random().toString(), ...ingredient }
+      { id: Math.random().toString(), ...ingredient },
     ]);
   };
 
+  const removeIngredientHandler = (ingredientId) => {
+    setUserIngredients((prevIngredients) =>
+      prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
+    );
+  };
+
   return (
-    <div className="App">
+    <div className='App'>
       <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        <IngredientList ingredients={userIngredients} onRemoveItem={() => {}} />
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeIngredientHandler}
+        />
       </section>
     </div>
   );
